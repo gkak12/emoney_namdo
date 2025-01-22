57dd3d1193cd23d70d10e8ac76e8d50e75b1129f
hyunjun <hyunjun5681@kakao.com>
2025-01-22 18:30:26 +0900

    Entity 객체 추가

---
+++ src/main/java/com/emoney/domain/entity/Emoney.java
@@ -0,0 +1,52 @@
+package com.emoney.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "EMONEY")
+@NoArgsConstructor
+public class Emoney {
+
+    @Id
+    @Column(name = "EMONEY_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emoneySeq;
+
+    @Column(name = "USER_SEQ")
+    private Long userSeq;
+
+    @Column(name = "ORDER_SEQ")
+    private Long orderSeq;
+
+    @Column(name = "TYPE_SEQ")
+    private Long typeSeq;
+
+    @Column(name = "AMOUNT")
+    private Long amount;
+
+    @Column(name = "USED_AMONUT")
+    private Long usedAmonut;
+
+    @Column(name = "REMAIN_AMOUNT")
+    private Long remainAmount;
+
+    @Column(name = "EXPIRATION_DATE")
+    private LocalDateTime expirationDate;
+
+    @Column(name = "IS_APPROVED")
+    private Boolean isApproved;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "emoney", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<EmoneyUsageHistory> usageHistory;
+}
---
+++ src/main/java/com/emoney/domain/entity/EmoneyUsageHistory.java
@@ -0,0 +1,31 @@
+package com.emoney.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "EMONEY_USAGE_HISTORY")
+@NoArgsConstructor
+public class EmoneyUsageHistory {
+
+    @Id
+    @Column(name = "EMONEY_USAGE_HISTORY_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emoneyUsageHistorySeq;
+
+    @Column(name = "USAGE_AMOUNT")
+    private Long usageAmount;
+
+    @Column(name = "CONTENT")
+    private Long content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "EMONEY_SEQ")
+    @JsonBackReference
+    private Emoney emoney;
+}
