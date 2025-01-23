6795460444f57306a0ea64b35dff047e28a9658d
hyunjun <hyunjun5681@kakao.com>
2025-01-23 18:21:49 +0900

    database 쿼리 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,9 +6,10 @@ CREATE TABLE EMONEY(
     AMOUNT BIGINT,                                  -- 적립금(초기 적립받은 금액)
     USAGE_AMOUNT BIGINT,                            -- 사용한 적립금
     REMAIN_AMOUNT BIGINT,                           -- 잔여 적립금
-    EXPIRATION_DATE TIMESTAMP,                      -- 만료일시
     IS_APPROVED BOOLEAN DEFAULT FALSE,              -- 승인여부
-    CONTENT VARCHAR(100)                            -- 내용
+    CONTENT VARCHAR(100),                           -- 내용
+    EXPIRATION_DATE TIMESTAMP,                      -- 만료일시
+    CREATION_DATE TIMESTAMP,                        -- 생성일시
 )
 
 CREATE TABLE EMONEY_USAGE_HISTORY(
@@ -16,6 +17,7 @@ CREATE TABLE EMONEY_USAGE_HISTORY(
     USAGE_TYPE_SEQ BIGINT,                                          -- 적립금 사용 타입 SEQ
     USAGE_AMOUNT BIGINT,                                            -- 사용한 적립금
     CONTENT VARCHAR(100),                                           -- 내용
+    CREATION_DATE TIMESTAMP,                                        -- 생성일시
     EMONEY_SEQ BIGINT,                                              -- 적립금 SEQ
     FOREIGN KEY (EMONEY_SEQ) REFERENCES EMONEY(EMONEY_SEQ)          -- 적립금 참조키
 )
