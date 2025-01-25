09aeb4e9237aea9f6b1a73fa14532344b1799b26
hyunjun <hyunjun5681@kakao.com>
2025-01-25 16:47:35 +0900

    적립금 사용내역 Repository 추가

---
+++ src/main/java/com/emoney/repository/EmoneyUsageHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.emoney.repository;
+
+import com.emoney.domain.entity.EmoneyUsageHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmoneyUsageHistoryRepository extends JpaRepository<Long, EmoneyUsageHistory>, EmoneyUsageHistoryRepositoryDsl {
+}
---
+++ src/main/java/com/emoney/repository/impl/EmoneyUsageHistoryRepositoryDslImpl.java
@@ -0,0 +1,12 @@
+package com.emoney.repository.impl;
+
+import com.emoney.repository.EmoneyUsageHistoryRepositoryDsl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class EmoneyUsageHistoryRepositoryDslImpl implements EmoneyUsageHistoryRepositoryDsl {
+}
