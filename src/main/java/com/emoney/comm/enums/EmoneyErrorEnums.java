32db9197ff35874d26ec3c6abc16006bbd1fa6df
hyunjun <hyunjun5681@kakao.com>
2025-01-27 19:19:34 +0900

    EmoneyErrorEnums 500 에러 추가

--- src/main/java/com/emoney/comm/enums/EmoneyErrorEnums.java
+++ src/main/java/com/emoney/comm/enums/EmoneyErrorEnums.java
@@ -8,12 +8,13 @@ public enum EmoneyErrorEnums {
     BAD_REQUEST(400, "잘못된 요청입니다."),
     UNAUTHENTICATED(401, "인증되지 않은 사용자 요청입니다."),
     PERMISSION_DENIED(403, "권한이 없는 사용자 요청입니다."),
-    NOT_FOUND(404, "해당 요청은 존재하지 않습니다.");
+    NOT_FOUND(404, "해당 요청은 존재하지 않습니다."),
+    INTERNAL_SERVER_ERROR(500, "서버 내부에서 에러가 발생했습니다.");
 
     private final int code;
     private final String msg;
 
-    private EmoneyErrorEnums(int code, String msg) {
+    EmoneyErrorEnums(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
