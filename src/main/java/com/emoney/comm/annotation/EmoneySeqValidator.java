369775f24a53623c7309fb11ec0b8d84747e8a42
hyunjun <hyunjun5681@kakao.com>
2025-01-28 23:48:37 +0900

    @PathVariable emoneySeq 유효성 검사 어노테이션 수정

--- src/main/java/com/emoney/comm/annotation/EmoneySeqValidator.java
+++ src/main/java/com/emoney/comm/annotation/EmoneySeqValidator.java
@@ -6,13 +6,8 @@ import jakarta.validation.ConstraintValidatorContext;
 public class EmoneySeqValidator implements ConstraintValidator<ValidEmoneySeq, Long> {
 
     @Override
-    public void initialize(ValidEmoneySeq constraintAnnotation) {
-        // 초기화 작업이 필요할 경우 작성 (없으면 비워두기)
-    }
-
-    @Override
     public boolean isValid(Long emoneySeq, ConstraintValidatorContext context) {
-        // emoneySeq가 null이거나 1 미만인 경우 유효하지 않음
-        return emoneySeq != null && emoneySeq >= 1;
+        // emoneySeq가 null 이거나 1 미만인 경우 유효하지 않음
+        return emoneySeq != null && emoneySeq > 0;
     }
 }
