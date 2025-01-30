d8e29b8360f724d6f53cdf240ccd2f2155e7fbac
hyunjun <hyunjun5681@kakao.com>
2025-01-31 02:58:55 +0900

    CREATION_DATE 메시지 수정

--- src/main/java/com/emoney/comm/enums/EmoneySearchEnums.java
+++ src/main/java/com/emoney/comm/enums/EmoneySearchEnums.java
@@ -9,7 +9,7 @@ public enum EmoneySearchEnums {
     USAGE_AMOUNT("usageAmount", "지금까지 사용한 누적 적립금입니다."),
     REMAIN_AMOUNT("remainAmount", "지금까지 사용하고 남은 잔액 적립금입니다."),
     EXPIRATION_DATE("expirationDate", "적립금 만료일입니다."),
-    CREATION_DATE("creationDate", "적립금 생성일입니다."),
+    CREATION_DATE("creationDate", "적립금 발급일입니다."),
     IS_APPROVED("isApproved", "적립금 승인 여부입니다."),
     IS_EXPIRED("isExpired", "적립금 만료 여부입니다.");
 
