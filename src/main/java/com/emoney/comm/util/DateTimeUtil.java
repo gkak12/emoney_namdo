565c78ab38361823e2de25d4ea9aebab2fefb84a
hyunjun <hyunjun5681@kakao.com>
2025-02-08 23:54:33 +0900

    DateTimeUtil 수정

--- src/main/java/com/emoney/comm/util/DateTimeUtil.java
+++ src/main/java/com/emoney/comm/util/DateTimeUtil.java
@@ -1,6 +1,7 @@
 package com.emoney.comm.util;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
@@ -12,15 +13,30 @@ public class DateTimeUtil {
                 .toLocalTime();
     }
 
+    public static String convertLocalTimeToString(String pattern, LocalTime localTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return formatter.format(localTime);
+    }
+
     public static LocalDate getLocalDate(){
         return ZonedDateTime
                 .now(ZoneId.of(timeZoneName))
                 .toLocalDate();
     }
 
+    public static String convertLocalDateToString(String pattern, LocalDate localDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return formatter.format(localDate);
+    }
+
     public static LocalDateTime getLocalDateTime(){
         return ZonedDateTime
                 .now(ZoneId.of(timeZoneName))
                 .toLocalDateTime();
+    }
+
+    public static String convertLocalDateTimeToString(String pattern, LocalDateTime localDateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return formatter.format(localDateTime);
     }
 }
