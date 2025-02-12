c9708a088902e775d61b3f1518c3d536102eb6fc
hyunjun <hyunjun5681@kakao.com>
2025-02-12 22:12:12 +0900

    엔티티 스키마 정의 추가

--- src/main/java/com/emoney/domain/entity/Emoney.java
+++ src/main/java/com/emoney/domain/entity/Emoney.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "EMONEY")
+@Table(name = "EMONEY", schema = "emoney_namdo")
 @Data
 @Builder
 @NoArgsConstructor
--- src/main/java/com/emoney/domain/entity/EmoneyUsageHistory.java
+++ src/main/java/com/emoney/domain/entity/EmoneyUsageHistory.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "EMONEY_USAGE_HISTORY")
+@Table(name = "EMONEY_USAGE_HISTORY", schema = "emoney_namdo")
 @Data
 @Builder
 @NoArgsConstructor
