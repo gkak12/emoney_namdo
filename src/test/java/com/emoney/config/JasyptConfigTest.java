5a57285abc9409f6018b2e598339e44734118b44
hyunjun <hyunjun5681@kakao.com>
2025-02-22 18:13:31 +0900

    Jasypt 테스트 코드 수정

--- src/test/java/com/emoney/config/JasyptConfigTest.java
+++ src/test/java/com/emoney/config/JasyptConfigTest.java
@@ -6,36 +6,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 public class JasyptConfigTest {
-
-    @Value("${jasypt.encryptor.password}")
-    private String encryptorPassword;
 
     @Test
     void stringEncryptor() {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        String password = "postgres12#$";
+        String url = "jdbc:h2:mem:meeting";
+        String username = "sa";
 
         System.out.println("url: ".concat(jasyptEncoding(url)));
         System.out.println("username: ".concat(jasyptEncoding(username)));
-        System.out.println("password: ".concat(jasyptEncoding(password)));
     }
 
     private String jasyptEncoding(String value) {
         StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
         pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(encryptorPassword);
+        pbeEnc.setPassword(System.getenv("JASYPT_KEY"));
 
         return pbeEnc.encrypt(value);
     }
 
     @Test
     void stringDecryptor() {
-        String url = "ENC(w8JYlfb55/e2xRgnXr1I80CiyxyYPRdteF6lVTQH1Jo9yrcr5LzhqQ==)";
-        String username = "ENC(uzYY3fWzGX4VSvJcNa7eEw==)";
+        String url = "fwst664/lJPSUzfOpnQyjLRwXi0KgGpvMRQG/eQ9Vz4=";
+        String username = "vs4ggM5iCn/MpoROqbLxjA==";
 
         System.out.println("url: ".concat(jasyptDecoding(url)));
         System.out.println("username: ".concat(jasyptDecoding(username)));
@@ -43,7 +36,8 @@ public class JasyptConfigTest {
 
     private String jasyptDecoding(String value) {
         StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setPassword(encryptorPassword);
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(System.getenv("JASYPT_KEY"));
 
         return pbeEnc.decrypt(
                 value
