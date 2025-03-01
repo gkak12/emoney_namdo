9c4501618cc714eb0206528f64be93a50e59d61d
hyunjun <hyunjun5681@kakao.com>
2025-03-01 21:52:40 +0900

    서비스 주석 추가

--- src/main/java/com/emoney/service/EmoneyService.java
+++ src/main/java/com/emoney/service/EmoneyService.java
@@ -9,14 +9,72 @@ import java.util.List;
 
 public interface EmoneyService {
 
-    List<ResponseEmoneyDto> findAllEmoneys();                                    // 적립금 전체 조회
-    ResponseEmoneyResponseListDto findPageEmoneys(RequestEmoneySearchDto emoneyRequestSearchDto);      // 적립금 페이징 조회
-    ResponseEmoneyResponseDetailDto findEmoneyDetail(Long emoneySeq);                    // 적립금 상세 조회
-    void createEmoney(RequestEmoneyCreateDto requestEmoneyCreateDto);                 // 적립금 지급
-    void deductEmoney(RequestEmoneyDeductDto requestEmoneyDeductDto);                 // 적립금 사용 및 차감
-    void useCancelEmoney(RequestEmoneyCancelDto requestEmoneyCancelDto);              // 적립금 사용 취소
-    void approveEmoney(Long emoneySeq);                                 // 적립금 승인
-    void rejectEmoney(Long emoneySeq);                                  // 적립금 반려
-    void extendEmoney(RequestEmoneyExtendDto requestEmoneyExtendDto);                 // 적립금 연장
-    void expireEmoney(Long emoneySeq);                                  // 적립금 만료
+    /**
+     * 적립금 전체 조회
+     *
+     * @return
+     */
+    List<ResponseEmoneyDto> findAllEmoneys();
+
+    /**
+     * 적립금 페이징 조회
+     *
+     * @param emoneyRequestSearchDto
+     * @return
+     */
+    ResponseEmoneyResponseListDto findPageEmoneys(RequestEmoneySearchDto emoneyRequestSearchDto);
+
+    /**
+     * 적립금 상세 조회
+     *
+     * @param emoneySeq
+     * @return
+     */
+    ResponseEmoneyResponseDetailDto findEmoneyDetail(Long emoneySeq);
+
+    /**
+     * 적립금 지급
+     *
+     * @param requestEmoneyCreateDto
+     */
+    void createEmoney(RequestEmoneyCreateDto requestEmoneyCreateDto);
+
+    /**
+     * 적립금 사용 및 차감
+     *
+     * @param requestEmoneyDeductDto
+     */
+    void deductEmoney(RequestEmoneyDeductDto requestEmoneyDeductDto);
+
+    /**
+     * 적립금 사용 취소
+     *
+     * @param requestEmoneyCancelDto
+     */
+    void useCancelEmoney(RequestEmoneyCancelDto requestEmoneyCancelDto);
+
+    /**
+     * 적립금 승인
+     *
+     * @param emoneySeq
+     */
+    void approveEmoney(Long emoneySeq);
+
+    /**
+     * 적립금 반려
+     * @param emoneySeq
+     */
+    void rejectEmoney(Long emoneySeq);
+
+    /**
+     * 적립금 연장
+     * @param requestEmoneyExtendDto
+     */
+    void extendEmoney(RequestEmoneyExtendDto requestEmoneyExtendDto);
+
+    /**
+     * 적립금 만료
+     * @param emoneySeq
+     */
+    void expireEmoney(Long emoneySeq);
 }
--- src/main/java/com/emoney/service/EmoneyUsageHistoryService.java
+++ src/main/java/com/emoney/service/EmoneyUsageHistoryService.java
@@ -6,6 +6,19 @@ import com.emoney.domain.dto.response.ResponseEmoneyUsageHistoryLogListDto;
 
 public interface EmoneyUsageHistoryService {
 
+    /**
+     * 사용자별 적립금 누적 사용금 페이징 조회
+     * 
+     * @param emoneyUsageHistorySearchDto
+     * @return
+     */
     ResponseEmoneyLogListDto findEmoneyTotalUsageAmountEachUser(RequestEmoneyUsageHistorySearchDto emoneyUsageHistorySearchDto);
+
+    /**
+     * 사용자별 적립금 사용내역 페이징 조회
+     * 
+     * @param emoneyUsageHistorySearchDto
+     * @return
+     */
     ResponseEmoneyUsageHistoryLogListDto findEmoneyUsageHistoryEachUser(RequestEmoneyUsageHistorySearchDto emoneyUsageHistorySearchDto);
 }
